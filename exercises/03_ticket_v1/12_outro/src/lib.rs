40a100655881044f4b400983a7b74c946f255354
dmx20070206 <dmx20070206@163.com>
2025-06-03 21:25:21 +0800

    ✨ feat: Day 1

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,77 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: i32,
+    unit_price: i32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: i32, unit_price: i32) -> Order {
+        if Order::valid_product_name(&product_name)
+            && Order::valid_quantity(&quantity)
+            && Order::valid_unit_price(&unit_price)
+        {
+            Order {
+                product_name,
+                quantity,
+                unit_price,
+            }
+        } else {
+            panic!("fuck you!");
+        }
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+    pub fn quantity(&self) -> &i32 {
+        &self.quantity
+    }
+    pub fn unit_price(&self) -> &i32 {
+        &self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        if Order::valid_product_name(&product_name) {
+            self.product_name = product_name;
+        }
+    }
+    pub fn set_quantity(&mut self, quantity: i32) {
+        if Order::valid_quantity(&quantity) {
+            self.quantity = quantity;
+        }
+    }
+    pub fn set_unit_price(&mut self, unit_price: i32) {
+        if Order::valid_unit_price(&unit_price) {
+            self.unit_price = unit_price;
+        }
+    }
+
+    pub fn total(&self) -> i32 {
+        self.quantity * self.unit_price
+    }
+
+    pub fn valid_product_name(product_name: &String) -> bool {
+        if product_name.len() == 0 {
+            panic!("fuck you!");
+        } else if product_name.len() > 300 {
+            panic!("fuck you!");
+        }
+        true
+    }
+    pub fn valid_quantity(quantity: &i32) -> bool {
+        if quantity <= &0 {
+            panic!("fuck you!");
+        }
+        true
+    }
+    pub fn valid_unit_price(unit_price: &i32) -> bool {
+        if unit_price <= &0 {
+            panic!("fuck you!");
+        }
+        true
+    }
+}
